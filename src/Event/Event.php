6c2712b1c6b32baa9d6363ca9f6c80db4ff25ec8
İzni Burak Demirtaş <izniburak@outlook.com>
2018-07-31 23:52:32 +0300

    Updated Event component.

--- src/Event/Event.php
+++ src/Event/Event.php
@@ -12,18 +12,18 @@ class Event
      * @param string $event
      * @param array $params
      * @param string $method
-     * @return void
+     * @return mixed
      */
     public function trigger($event, Array $params = [], $method = 'handle')
     {
         $listeners 	= config('services.listeners');
         foreach ($listeners[$event] as $listener) {
             if (!class_exists($listener)) {
-                throw new ExceptionHandler('Listener class not found.', $listener);
+                throw new ExceptionHandler('Event class not found.', $listener);
             }
 
             if (!method_exists($listener, $method)) {
-                throw new ExceptionHandler('Method not found in Listener class.', $listener . '::' . $method . '()');
+                throw new ExceptionHandler('Method not found in Event class.', $listener . '::' . $method . '()');
             }
 
             return call_user_func_array([new $listener, $method], $params);
