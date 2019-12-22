c61c920d22fe09688c893ded6bf300e4c3e7f0c0
İzni Burak Demirtaş <izniburak@outlook.com>
2019-12-23 01:41:04 +0300

    Added Config facade.

---
+++ src/Facades/Config.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Nur\Facades;
+
+use Nur\Kernel\Facade;
+
+/**
+ * @method static bool has($key)
+ * @method static mixed get($key, $default = null)
+ * @method static array all()
+ * @method static void set($key, $value = null)
+ * @method static void prepend($key, $value)
+ * @method static void push($key, $value)
+ *
+ * @see \Nur\Config\Config
+ */
+class Config extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'config';
+    }
+}
