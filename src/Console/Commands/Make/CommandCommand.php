2faa72ad5ba6c2bd17d7e443f00d1abf51a2b26d
İzni Burak Demirtaş <izniburak@outlook.com>
2020-01-13 10:18:35 +0300

    Updated make:command template.

--- src/Console/Commands/Make/CommandCommand.php
+++ src/Console/Commands/Make/CommandCommand.php
@@ -51,11 +51,18 @@ class CommandCommand extends Command
 namespace App\Console\Commands;
 
 use Nur\Console\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\{
+    Input\InputInterface,
+    Output\OutputInterface
+};
 
 class $className extends Command
 {
+    /**
+     * Configure the command
+     *
+     * @return void
+     */
     protected function configure()
     {
         \$this
@@ -64,6 +71,14 @@ class $className extends Command
             ->setHelp('Help description of the Command');
     }
 
+    /**
+     * Execute the command
+     * 
+     * @param InputInterface  \$input
+     * @param OutputInterface \$output
+     *
+     * @return mixed|void
+     */
     protected function execute(InputInterface \$input, OutputInterface \$output)
     {
         // your codes...
